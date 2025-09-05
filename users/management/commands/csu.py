f7ee3e00e778aba3584230aa233bc9f01cfd726e
Anastasiya Soloviova <mafanya.as@mail.ru>
2025-09-05 10:58:00 +0300

    check user

---
+++ users/management/commands/csu.py
@@ -0,0 +1,15 @@
+from django.core.management import BaseCommand
+from users.models import User
+
+
+class Command(BaseCommand):
+    """
+    Команда для создания суперпользователя.
+    """
+    def handle(self, *args, **kwargs):
+        user = User.objects.create(email="moderator@example.com")
+        user.is_staff = True
+        user.is_active = True
+        user.is_superuser = True
+        user.set_password("123321try")
+        user.save()

0ecfa319334eb6881d0a0ae973c28ce6bda178f9
Anastasiya Soloviova <mafanya.as@mail.ru>
2025-09-11 16:07:30 +0300

    fix tg messaging

--- habits/tasks.py
+++ habits/tasks.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from django.utils import timezone
 from celery import shared_task
 from habits.models import Habit
 from habits.services import send_telegram_message
@@ -7,9 +7,9 @@ from habits.services import send_telegram_message
 @shared_task()
 def telegram_notification():
     habits = Habit.objects.all()
-    current_date = datetime.datetime.now()  # Текущее время
+    current_date = timezone.localtime().time()
     for habit in habits:
-        if habit.time >= current_date:
+        if habit.time <= current_date:
             chat_id = habit.user.tg_chat_id
             message = f"Я буду {habit.action} в {habit.time} в {habit.place}."
             send_telegram_message(chat_id, message)
